ae621ba75dd276864174a84846e68a8d949c55d4
VisionEP1 <grischa.hauser@gmail.com>
2017-02-09 22:23:50 +0100

    All Source

---
+++ APM-Monitor/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace APM_Monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Clicks";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(94, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(33, 109);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Keys";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(30, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "APM";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(94, 149);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(94, 184);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(33, 184);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(30, 13);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "CPM";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(94, 221);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "0";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(33, 221);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 13);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "KPM";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(239, 36);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(977, 546);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(36, 320);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(37, 13);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "Status";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(98, 320);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(37, 13);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Status";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1237, 558);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Save Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1359, 708);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "APM Monitor - By GoodOldVision - League Of Legends Version";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ APM-Monitor/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APM_Monitor
+{
+    public partial class Form1 : Form
+    {
+
+
+
+      
+        public static int[] click_counte = new int[100];
+        public static int[] key_counter = new int[100];
+      public  static bool running = false;
+       public static Stopwatch stopwatch;
+        Thread starter,draw;
+        static int limit = 100;
+
+
+      
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Shown += Form1_Shown;
+            this.FormClosing += Form_FormClosing;
+          
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            //    Start();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            chart1.Series.Clear();
+
+
+            Keyboard.start();
+            Mouse.start();
+            //      Subscribe();
+            starter = new Thread(Start);
+            starter.Start();
+            draw = new Thread(Draw);
+            draw.Start();
+        }
+
+        void Form_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            starter.Abort();
+            draw.Abort();
+
+            if (e.CloseReason == CloseReason.UserClosing) { }
+             
+                if (e.CloseReason == CloseReason.WindowsShutDown) { }
+  
+        }
+
+        public void Start()
+        {
+            MethodInvoker inv = delegate
+            {
+                label14.Text = "Wait for new Game";
+            };
+            this.Invoke(inv);
+
+        
+            bool found = false;
+            while (!found)
+            {
+                Process[] pname = Process.GetProcessesByName("League of Legends");
+                if (pname.Length == 0)
+
+                {
+                    Thread.Sleep(5000);
+                }
+                else
+                { found = true; }
+            }
+            Thread.Sleep(30000);
+            MethodInvoker inv2 = delegate
+            {
+                label14.Text = "recording";
+            };
+            this.Invoke(inv2);
+
+            stopwatch  = new Stopwatch();
+            stopwatch.Start();
+            running = true;
+         
+
+  
+            while (found)
+            {
+                Process[] pname = Process.GetProcessesByName("League of Legends");
+                if (pname.Length == 0)
+
+                { found = false; }
+                else
+                { Thread.Sleep(10000); }
+            }
+            MethodInvoker inv3 = delegate
+            {
+                label14.Text = "Wait for new Game";
+            };
+            this.Invoke(inv3);
+            Stop();
+
+        }
+
+        public void Stop() {
+
+
+
+
+            Display();
+
+        
+
+            stopwatch.Stop();
+            running = false;
+
+            click_counte = new int[100];
+           key_counter = new int[100];
+            limit = 100;
+
+
+            Start();
+        }
+        public void Display() {
+            //todo
+            limit = (int)(stopwatch.ElapsedMilliseconds / 60000);
+            MethodInvoker inv4 = delegate
+            {
+          
+            TimeSpan ts = stopwatch.Elapsed;
+            label2.Text = ts.ToString(@"m\:ss");
+            int clicksTotal = 0;
+            int keysTotal = 0;
+
+
+
+            chart1.Series.Clear();
+               
+
+                chart1.Series.Add("APM");
+            chart1.Series.Add("CPM");
+            chart1.Series.Add("KPM");
+
+
+                chart1.ChartAreas[0].AxisX.Maximum = limit;
+
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+                for (int i = 0; i <= limit; i++)
+            {
+                clicksTotal += click_counte[i];
+                keysTotal += key_counter[i];
+                chart1.Series["APM"].Points.AddXY(i, click_counte[i] + key_counter[i]);
+                chart1.Series["CPM"].Points.AddXY(i, click_counte[i] );
+                chart1.Series["KPM"].Points.AddXY(i,  key_counter[i]);
+            }
+
+            chart1.Series["CPM"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["KPM"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series["APM"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            label3.Text = clicksTotal.ToString();
+            label5.Text = keysTotal.ToString();
+
+            double apm = (double)(clicksTotal + keysTotal) * 60 / (double)(stopwatch.ElapsedMilliseconds / 1000);
+            double cpm = (double)(clicksTotal ) * 60 / (double)(stopwatch.ElapsedMilliseconds / 1000);
+            double kpm = (double)(keysTotal) * 60 / (double)(stopwatch.ElapsedMilliseconds / 1000);
+            label8.Text = apm.ToString("F");
+            label9.Text = cpm.ToString("F");
+            label11.Text = kpm.ToString("F");
+
+              
+            };
+            this.Invoke(inv4);
+
+
+        }
+ 
+    
+        public void Draw()
+        {
+           
+            if(running)    Display();
+            Thread.Sleep(60000);
+            Draw();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+               chart1.SaveImage(savefile.FileName, System.Windows.Forms.DataVisualization.Charting.ChartImageFormat.Png);
+            }
+   
+        }
+    }
+}
